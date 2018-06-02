b5607d568ec3302e08c5b88baa0aaaa7a82e0cb1
barbarossa92 <donnietruth@gmail.com>
2018-06-02 15:37:49 +0600

    fix graph import func && added new graph visualization lib

--- main/common/models.py
+++ main/common/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class BaseModel(models.Model):
     eid = models.CharField(max_length=2)
-    graph = models.ForeignKey('Graph')
+    graph = models.ForeignKey('Graph', on_delete=models.CASCADE)
     text = models.TextField()
 
     class Meta:
--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.shortcuts import render
 from django.views.generic import View, DetailView
 from main.models import Graph, Node, Group
@@ -12,6 +13,7 @@ class ListView(View):
         return render(request, "graph_list.html", {"graphs": Graph.objects.all(),
                                                    "form": self.form()})
 
+    @transaction.atomic
     def post(self, request):
         form = self.form(request.POST, request.FILES)
         if form.is_valid():
@@ -19,22 +21,20 @@ class ListView(View):
             groups_list = form.cleaned_data.get("groups")
             if nodes_list and groups_list:
                 graph = Graph.objects.create()
+                created_groups = list()
                 nodes = [Node(eid=i.get("eid"), text=i.get("text"), graph=graph) for i in nodes_list]
-                Node.objects.bulk_create(nodes)
                 for group in groups_list:
                     group_obj = Group.objects.create(eid=group.get("eid"), text=group.get("text"), graph=graph)
+                    created_groups.append(group_obj)
                     groupped = group.get("groupped")
                     if groupped:
-                        free_nodes = graph.node_set.filter(eid__in=groupped.split(), parent_id__isnull=True)
-                        free_groups = graph.group_set.filter(eid__in=groupped.split(), parent_id__isnull=True)
-                        free_elements = list(free_nodes.values_list('eid', flat=True)) + \
-                                         list(free_groups.values_list('eid', flat=True))
-                        free_nodes.update(parent=group_obj)
-                        free_groups.update(parent=group_obj)
-                        diff_eids = list(set(groupped.split()).difference(set(free_elements)))
+                        free_elements = list(filter(lambda n: n.eid in groupped.split() and not n.parent, nodes + created_groups))
+                        for element in free_elements:
+                            element.parent = group_obj
+                            element.save()
+                        diff_eids = list(set(groupped.split()).difference(set([i.eid for i in free_elements])))
                         if diff_eids:
-                            exist_elements = list(graph.group_set.filter(eid__in=diff_eids)) + \
-                                             list(graph.node_set.filter(eid__in=diff_eids))
+                            exist_elements = list(filter(lambda el: el.eid in diff_eids, nodes + created_groups))
                             for obj in exist_elements:
                                 tmp_obj = obj
                                 while tmp_obj.parent != None:
